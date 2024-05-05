30b81651a561d04cdc053b0826bb63c6e9d35866
DGTV11 <danielweedk@gmail.com>
2024-05-05 12:39:21 +0800

    Started working on navbar
    Co-authored-by: lcclcoder <lcclcoder@users.noreply.github.com>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,27 @@
-<ul><a><a href="/">Home</a></a><a><a href="/about">About</a></ul>
-<slot>
+<script>
+	const routes = [
+		{ href: '/', label: 'Home' },
+		{ href: '/suggestions', label: 'Suggestions' },
+		{ href: '/voting', label: 'Voting' },
+		{ href: '/history', label: 'History' },
+		{ href: '/news', label: 'News' }
+	];
+</script>
 
-</slot>
+<nav>
+	{#each routes as route}
+		<a href={route['href']}>{route['label']}</a>
+	{/each}
+</nav>
+
+<slot />
+
+<!--TODO Fix up styling WITH NO Codeium AND minimal copy-paste-->
+<style>
+	nav {
+		display: flex;
+	}
+	a {
+		flex: 1;
+	}
+</style>
